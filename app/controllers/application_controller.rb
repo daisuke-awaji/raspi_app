4e6610cbe2c0011a18f36056627643c3136c0d16
daisuke-awaji <daisuke.awaji@i.softbank.jp>
2017-10-21 07:14:40 +0000

    gem install PiPiper.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
+# coding: utf-8
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def on
+    pin_17 = PiPiper::Pin.new :pin => 17, :direction => :out
+
+    1.times do
+      pin_17.on
+      sleep 1
+      pin_17.off
+      sleep 1
+    end
+
+    # 17番ピンの使用をやめる
+    uexp = open("/sys/class/gpio/unexport", "w")
+    uexp.write(17)
+    uexp.close
+    
+    render html: "hello Light up on rails. 17番ピンに命令しました。"
+  end
+
 end
